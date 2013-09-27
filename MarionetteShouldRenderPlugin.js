f730c40b32c90b306dc64115257cd242496d8483
Martin Hewitt <martin.hewitt@surevine.com>
2013-09-27 09:35:19 +0100

    Added underscore as dependency and added hard check for presence of shouldRender detection

--- MarionetteShouldRenderPlugin.js
+++ MarionetteShouldRenderPlugin.js
@@ -1,15 +1,15 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['marionette'], factory);
+        define(['marionette', 'underscore'], factory);
     } else if (typeof exports === 'object') {
         // Node/CommonJS
-        factory(require('marionette'));
+        factory(require('marionette'), require('underscore'));
     } else {
         // Browser globals
-        factory(Backbone.Marionette);
+        factory(Backbone.Marionette, _);
     }
-}(function(Marionette) {
+}(function(Marionette, _) {
     
     var helper = {
         checkShouldRender: function() {
@@ -25,7 +25,7 @@
     var originalView = Marionette.View
     Marionette.View = originalView.extend(_.extend({
         render: function() {
-            if ( !this.checkShouldRender() ) {
+            if ( !_.isFunction(this.checkShouldRender) || !this.checkShouldRender() ) {
                 return;
             }
             
@@ -36,7 +36,7 @@
     var originalRegion = Marionette.Region
     Marionette.Region = originalRegion.extend(_.extend({
         render: function() {
-            if ( !this.checkShouldRender() ) {
+            if ( !_.isFunction(this.checkShouldRender) || !this.checkShouldRender() ) {
                 return;
             }
             
@@ -47,7 +47,7 @@
     var originalCollectionView = Marionette.CollectionView
     Marionette.CollectionView = originalCollectionView.extend(_.extend({
         render: function() {
-            if ( !this.checkShouldRender() ) {
+            if ( !_.isFunction(this.checkShouldRender) || !this.checkShouldRender() ) {
                 return;
             }
             
@@ -58,7 +58,7 @@
     var originalItemView = Marionette.ItemView
     Marionette.ItemView = originalItemView.extend(_.extend({
         render: function() {
-            if ( !this.checkShouldRender() ) {
+            if ( !_.isFunction(this.checkShouldRender) || !this.checkShouldRender() ) {
                 return;
             }
             
@@ -69,7 +69,7 @@
     var originalLayout = Marionette.Layout
     Marionette.Layout = originalLayout.extend(_.extend({
         render: function() {
-            if ( !this.checkShouldRender() ) {
+            if ( !_.isFunction(this.checkShouldRender) || !this.checkShouldRender() ) {
                 return;
             }
             
@@ -80,7 +80,7 @@
     var originalCompositeView = Marionette.CompositeView
     Marionette.CompositeView = originalCompositeView.extend(_.extend({
         render: function() {
-            if ( !this.checkShouldRender() ) {
+            if ( !_.isFunction(this.checkShouldRender) || !this.checkShouldRender() ) {
                 return;
             }
             
